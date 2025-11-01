77d03478f0b987a65d075ac35751f631661a7f89
arukayies <arukayies@users.noreply.github.com>
2025-11-01 12:32:05 +0900

    Fix: Adjust baseURL and publishDir for GitHub Pages, correct index.json path

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ function initLunr() {
 
     return new Promise((resolve, reject) => {
     var request = new XMLHttpRequest();
-    request.open("GET", "../index.json", true);
+    request.open("GET", "/homepage/index.json", true);
 
         request.onload = function () {
             if (request.status >= 200 && request.status < 400) {
